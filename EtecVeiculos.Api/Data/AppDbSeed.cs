d92ef2de3442f82efe991666ebe12eac801cb3dd
JoseMaksud <jf9718830@gmail.com>
2024-06-18 20:04:29 -0300

    Feito o controller e models das marcas e modelos

--- EtecVeiculos.Api/Data/AppDbSeed.cs
+++ EtecVeiculos.Api/Data/AppDbSeed.cs
@@ -24,5 +24,39 @@ public class AppDbSeed
         ];
         modelBuilder.Entity<TipoVeiculo>().HasData(tipoVeiculos);
         #endregion
+
+        #region Marcas
+        List<Marca> marcas = new() {
+            new() {
+                Id = 1,
+                Name = "Porsche",
+            },
+            new() {
+                Id = 2,
+                Name = "Ferrari",
+            },
+            new() {
+                Id = 3,
+                Name = "Bugatti",
+            }
+        };
+        modelBuilder.Entity<Marca>().HasData(marcas);
+        #endregion
+
+        #region Modelo
+        List<Modelo> modelos = new() {
+            new() {
+                Id = 1,
+                Name = "Porsche 911 GT3 RS",
+                MarcaId = 1,
+            },
+            new() {
+                Id = 2,
+                Name = "Ferrari 296 GTB",
+                MarcaId = 2,
+            },
+        };
+        modelBuilder.Entity<Modelo>().HasData(modelos);
+        #endregion
     }
 }
